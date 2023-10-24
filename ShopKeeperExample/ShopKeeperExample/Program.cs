b8fbc5d5127f392dc9f614f1e2cc74f4f54b75d6
Scott Foster <scottgarryfoster@googlemail.com>
2023-10-24 19:41:15 +0100

    Added basic console application

---
+++ ShopKeeperExample/ShopKeeperExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShopKeeperExample
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+    }
+}
